9d6b066c3e5d0fb559e607b131f07decf6efa575
Leo MPB <vanleotran@gmail.com>
2022-02-07 10:23:40 +0200

    added info to printf.txt [skip ci]

--- ft_printf/ft_printf.c
+++ ft_printf/ft_printf.c
@@ -6,15 +6,38 @@
 /*   By: leo <leo@student.42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/06 08:55:01 by leo               #+#    #+#             */
-/*   Updated: 2022/02/06 08:56:41 by leo              ###   ########.fr       */
+/*   Updated: 2022/02/06 15:27:03 by leo              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libftprintf.h"
 #include "libft/libft.h"
+#include <stdarg.h>
 
 int	ft_printf(const char *format, ...)
 {
-	
+	va_list args;
+	va_start(args, format);
+	int num_args = ft_strlen(format);
+
+	for (int i = 0; i < num_args; i++)
+	{
+		if (format[i] == '%d')
+		{
+			int x = va_arg(args, int);
+			printf("%d\n", x);
+		}
+		else if (format[i] == 'f')
+		{
+			double x = va_arg(args, double);
+			printf("%f\n", x);
+		}
+	}
+	va_end(args);
 	return (1);
+}
+
+int main (void)
+{
+	ft_printf("hej %d c %c", 5, 'c');
 }
